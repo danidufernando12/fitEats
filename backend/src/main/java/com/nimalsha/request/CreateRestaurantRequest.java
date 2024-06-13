41a6324ad9ea46d56dec5dbde5983a4613e41425
kasuni2001-maker <136631162+kasuni2001-maker@users.noreply.github.com>
2024-06-13 06:06:22 -0800

    initial commit

---
+++ backend/src/main/java/com/nimalsha/request/CreateRestaurantRequest.java
@@ -0,0 +1,20 @@
+package com.nimalsha.request;
+
+import com.nimalsha.model.Address;
+import com.nimalsha.model.ContactInformation;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateRestaurantRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private String cuisineType;
+    private Address address;
+    private ContactInformation contactInformation;
+    private String openingHours;
+    private List<String> images;
+
+}
